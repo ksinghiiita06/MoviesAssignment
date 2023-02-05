6dce5c2bebd433c3734b29579578f3b943bb0069
kuldeep singh <kuldeep.singh05@nagarro.com>
2023-02-05 19:55:40 +0530

    changed Arabic to actual arabic text

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,5 @@
 export const PERSIST_STATE = 'persist/REHYDRATE';
 export const POSTER_BASE_URL = 'https://image.tmdb.org/t/p/original/';
 export const ENGLISH = 'English';
-export const ARABIC = 'Arabic';
+export const ARABIC = 'عربي';
+// export const ARABIC = 'Arabic';
